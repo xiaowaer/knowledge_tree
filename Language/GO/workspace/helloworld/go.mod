10b1c254079d999e67a02eb0312d3fc858d2099b
xiaoliwaer <1011388302@qq.com>
2022-08-24 14:14:55 +0800

    ok

---
+++ Language/GO/workspace/helloworld/go.mod
@@ -0,0 +1,5 @@
+module example.com/hello_work
+
+go 1.18
+
+require github.com/georgehao/gomodtestc v1.0.1 // indirect
---
+++ Language/GO/workspace/helloworld/helloworld.go
@@ -0,0 +1,13 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+
+	"github.com/georgehao/gomodtestc"
+)
+
+    func main() {
+
+            fmt.Println(gomodtestc.PrintStr("Hello", 100))
+        }
